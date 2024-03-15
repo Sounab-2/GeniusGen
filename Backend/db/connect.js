c692ee6c5ccc0741e7468fd61d5d0776dd1c1b12
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-03-15 13:22:51 +0530

    Revert "Added .env to .gitignore"
    This reverts commit 9801c1c5b935467b5d7a80d2b49d3e16b37eb3d5.

--- Backend/db/connect.js
+++ Backend/db/connect.js
@@ -1,3 +1,34 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = (url) => {
+//   return mongoose.connect(url);
+// };
+
+// module.exports = connectDB;
+
+
+// const mongoose = require('mongoose');
+
+// // // Fix Mongoose deprecation warning
+//  mongoose.set('strictQuery', false);
+
+// const connectDB = async (url) => {
+//   try {
+//     await mongoose.connect(url, {
+//       useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//         useCreateIndex: true
+//     });
+//     console.log('Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error.message);
+//     throw error; // Rethrow the error for better error handling in the calling code
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
 const connectDB = async (mongoURI) => {
