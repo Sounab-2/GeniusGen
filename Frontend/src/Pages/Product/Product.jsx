061444d0bb13bd07ff7963643f6096794f52a933
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-03-31 00:32:32 +0530

    Quiz button fixed

--- Frontend/src/Pages/Product/Product.jsx
+++ Frontend/src/Pages/Product/Product.jsx
@@ -20,7 +20,7 @@ const Product = () => {
   const [histories, setHistories] = useState([]); // Initialize histories state
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.user && state.isLoading);
-
+  
   
 
 
@@ -109,9 +109,9 @@ const Product = () => {
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
             </button>
           </div>
-          <div className=' h-full w-auto  flex items-center justify-center'>
+          { text!=='' && (<div className=' h-full w-auto  flex items-center justify-center'>
             <Link to="/quiz" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center  w-64 h-12 relative z-40">Test your knowledge with a Quiz</Link>
-          </div>
+          </div>)}
         </div>
 
 
