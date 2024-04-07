e1069689c56f83a754299e0dbd0914248fc9b345
snikdhendu <snikdhendupramanik@gmail.com>
2024-04-07 23:44:01 +0530

    minor design issue fixed #2

--- Frontend/src/Pages/About/About.jsx
+++ Frontend/src/Pages/About/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
               <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full  lg:h-12 lg:w-12 bg-primary-900">
                 <svg class="w-5 h-5 lg:w-6 lg:h-6 text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
               </div>
-              <h3 class="mb-2 text-xl font-boldtext-white">Personalized Learning Paths</h3>
+              <h3 class="mb-2 text-xl font-bold text-white">Personalized Learning Paths</h3>
               <p class="text-gray-400">Receive tailored recommendations for blog posts and quizzes based on your individual interests and performance, ensuring a personalized learning experience.</p>
             </div>
             <div>
